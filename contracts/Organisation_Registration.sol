6f06fa177e8144b278199e061f759bbf96713817
UnleashedIcon <kkipkurui123@gmail.com>
2024-01-12 23:21:27 +0300

    add organisation registration contract and stakeholder management smart contracts

---
+++ contracts/Organisation_Registration.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract OrganizationRegistration {
+    address public owner;
+    MyToken private organizationToken;
+    struct Organization {
+        string orgName;
+        address orgAddress;
+        MyToken tokenContract;
+    }
+
+    mapping(address => Organization) public organizations;
+
+    event OrganizationRegistered(string indexed orgName, address indexed orgAddress, address tokenContract);
+    event OrganizationRemoved(string indexed orgName, address indexed orgAddress);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor(address tokenContractAddress) {
+        owner = msg.sender;
+        organizationToken = MyToken(tokenContractAddress); // Adjust here
+
+    }
+    function registerOrganization(string memory _orgName, string memory _tokenName, string memory _tokenSymbol) external onlyOwner {
+        require(organizations[msg.sender].orgAddress == address(0), "Organization already registered");
+
+        MyToken newToken = new MyToken(_tokenName, _tokenSymbol, msg.sender);
+        organizations[msg.sender] = Organization({
+            orgName: _orgName,
+            orgAddress: msg.sender,
+            tokenContract: newToken
+        });
+
+        emit OrganizationRegistered(_orgName, msg.sender, address(newToken));
+    }
+
+    function removeOrganization(address organizationAddress) external onlyOwner {
+        require(organizations[organizationAddress].orgAddress != address(0), "Organization not found");
+        delete organizations[organizationAddress];
+        emit OrganizationRemoved(organizations[msg.sender].orgName, msg.sender);
+    }
+
+    function getTokenContractAddress(address organizationAddress) external view returns (address) {
+        return address(organizations[organizationAddress].tokenContract);
+    }
+
+    function getOrganizationDetails(address orgAddress) external view returns (string memory orgName, address organizationAddress, address tokenContract) {
+        Organization storage org = organizations[orgAddress];
+        require(org.orgAddress != address(0), "Organization not found");
+        return (org.orgName, org.orgAddress, address(org.tokenContract));
+    }
+
+    function getTokenBalance(address organizationAddress) external view returns (uint256) {
+        Organization storage org = organizations[organizationAddress];
+        require(org.orgAddress != address(0), "Organization not found");
+        
+        return organizationToken.balanceOf(organizationAddress); // Adjust here
+    }
+
+    function transferTokens(address organizationAddress, address to, uint256 value) external {
+        Organization storage org = organizations[organizationAddress];
+        require(org.orgAddress != address(0), "Organization not found");
+
+
+        uint balance = organizationToken.balanceOf(organizationAddress);
+        console.log(balance);
+        require(organizationToken.balanceOf(organizationAddress) >= value, "Insufficient balance");
+
+        organizationToken.transfer(to, value);
+    }
+}
+
+
+contract MyToken is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address initialOwner
+    ) ERC20(name, symbol) Ownable(initialOwner) {
+        _mint(initialOwner, 1000 * (10**18));
+    }
+}
+
---
+++ contracts/StakeholderManagement.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./Organisation_Registration.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
+
+
+interface OrganizationRegistrationInterface {
+    function getTokenContractAddress(address organizationAddress) external view returns (address);
+}
+contract StakeholderManagement {
+    OrganizationRegistration public organizationRegistrationContract;
+    MyToken public tokenContract;
+    address public owner;
+
+    enum StakeholderType { Community, Investor, PresaleBuyer, Founder, Other }
+
+    struct VestingSchedule {
+        uint256 startTime;
+        uint256 duration;
+        uint256 endVestingTime;
+        uint256 initialAmount;
+        StakeholderType stakeholderType;
+    }
+
+    struct BeneficiaryDetails {
+        address beneficiaryAddress;
+        VestingSchedule vestingDetails;
+    }
+
+    mapping(address => mapping(address => bool)) public whitelistedAddresses;
+    mapping(address => mapping(address => VestingSchedule)) public organizationVestingSchedules;
+    mapping(address => BeneficiaryDetails[]) public organizationBeneficiaries;
+    mapping(address => address[]) public organizationWhitelistedAddresses;
+
+    event VestingScheduled(
+        address indexed organization,
+        address indexed beneficiary,
+        uint256 startTime,
+        uint256 duration,
+        uint256 endVestingTime,
+        uint256 initialAmount,
+        StakeholderType stakeholderType
+    );
+    event Whitelisted(
+        address indexed organization,
+        address indexed beneficiary
+    );
+
+    constructor(address _organizationRegistrationContract) {
+        owner = msg.sender;
+        organizationRegistrationContract = OrganizationRegistration(_organizationRegistrationContract);
+        address tokenContractAddress = organizationRegistrationContract.getTokenContractAddress(owner);
+        tokenContract = MyToken(tokenContractAddress);
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+  
+
+    modifier hasVestingSchedule(address beneficiaryAddress) {
+        require(organizationVestingSchedules[owner][beneficiaryAddress].startTime != 0, "No vesting schedule found");
+        _;
+    }
+
+    function addVestingSchedule(
+        address _beneficiary,
+        uint256 _startTime,
+        uint256 _duration,
+        uint256 _initialAmount,
+        StakeholderType _stakeholderType
+    ) external onlyOwner {
+        require(_duration > 0, "Invalid duration");
+        require(_beneficiary != address(0), "Invalid beneficiary");
+        require(organizationVestingSchedules[msg.sender][_beneficiary].startTime == 0, "Schedule already exists");
+
+        uint256 endVestingTime = _startTime + _duration;
+
+        organizationVestingSchedules[msg.sender][_beneficiary] = VestingSchedule({
+            startTime: _startTime,
+            duration: _duration,
+            endVestingTime: endVestingTime,
+            initialAmount: _initialAmount,
+            stakeholderType: _stakeholderType
+        });
+
+        organizationBeneficiaries[msg.sender].push(BeneficiaryDetails({
+            beneficiaryAddress: _beneficiary,
+            vestingDetails: organizationVestingSchedules[msg.sender][_beneficiary]
+        }));
+
+        emit VestingScheduled(msg.sender, _beneficiary, _startTime, _duration, endVestingTime, _initialAmount, _stakeholderType);
+    }
+
+   function whitelistAddress(address _beneficiary) external onlyOwner {
+        require(_beneficiary != address(0), "Invalid address");
+        whitelistedAddresses[msg.sender][_beneficiary] = true;
+        organizationWhitelistedAddresses[msg.sender].push(_beneficiary);
+
+        emit Whitelisted(msg.sender, _beneficiary);
+    }
+
+    function getWhitelistedAddresses(address _organizationAddress) external view returns (address[] memory) {
+        return organizationWhitelistedAddresses[_organizationAddress];
+    }
+
+    function getVestingSchedules(address _organizationAddress) external view returns (BeneficiaryDetails[] memory) {
+        return organizationBeneficiaries[_organizationAddress];
+    }
+
+    function getOrganizationAndVestingSchedules(address _beneficiaryAddress) external view returns (VestingSchedule memory) {
+    return organizationVestingSchedules[owner][_beneficiaryAddress];
+    }
+
+    function isAddressWhitelisted(address _beneficiaryAddress) external view returns (bool) {
+        return whitelistedAddresses[owner][_beneficiaryAddress];
+    }
+    
+    function removeWhitelistedAddress(address _beneficiary) external onlyOwner {
+        require(whitelistedAddresses[msg.sender][_beneficiary], "Address not whitelisted");
+        
+        for (uint256 i = 0; i < organizationWhitelistedAddresses[msg.sender].length; i++) {
+            if (organizationWhitelistedAddresses[msg.sender][i] == _beneficiary) {
+                organizationWhitelistedAddresses[msg.sender][i] = organizationWhitelistedAddresses[msg.sender][organizationWhitelistedAddresses[msg.sender].length - 1];
+                organizationWhitelistedAddresses[msg.sender].pop();
+                break;
+            }
+        }
+        
+        delete whitelistedAddresses[msg.sender][_beneficiary];
+    }
+
+    function removeBeneficiary(address _organizationAddress, address _beneficiary) external onlyOwner {
+        require(organizationVestingSchedules[_organizationAddress][_beneficiary].startTime != 0, "No vesting schedule found for the beneficiary");
+        
+        for (uint256 i = 0; i < organizationBeneficiaries[_organizationAddress].length; i++) {
+            if (organizationBeneficiaries[_organizationAddress][i].beneficiaryAddress == _beneficiary) {
+                organizationBeneficiaries[_organizationAddress][i] = organizationBeneficiaries[_organizationAddress][organizationBeneficiaries[_organizationAddress].length - 1];
+                organizationBeneficiaries[_organizationAddress].pop();
+                break;
+            }
+        }
+        
+        delete organizationVestingSchedules[_organizationAddress][_beneficiary];
+    }
+
+    function claimTokens(address beneficiaryAddress) external hasVestingSchedule(beneficiaryAddress) {
+        require(msg.sender == owner || whitelistedAddresses[owner][beneficiaryAddress], "Not authorized to claim tokens");
+        
+        VestingSchedule storage vesting = organizationVestingSchedules[owner][beneficiaryAddress];
+        require(block.timestamp >= vesting.endVestingTime, "Vesting period not ended");
+        
+        uint256 claimableTokens = calculateClaimableTokens(vesting.initialAmount, vesting.endVestingTime, vesting);
+        require(claimableTokens > 0, "No tokens to claim");
+
+        uint256 balance = organizationRegistrationContract.getTokenBalance(owner);
+        console.log(balance);
+        require(balance >= claimableTokens, "Insufficient token balance in contract");
+
+        organizationRegistrationContract.transferTokens(owner, beneficiaryAddress, claimableTokens);
+
+    }
+
+    function calculateClaimableTokens(uint256 initialAmount, uint256 endVestingTime, VestingSchedule storage vesting) internal view returns (uint256) {
+        if (block.timestamp >= endVestingTime) {
+            return initialAmount;
+        } else {
+            uint256 timePassed = block.timestamp - vesting.startTime;
+            uint256 totalVestingTime = vesting.duration;
+            return (initialAmount * timePassed) / totalVestingTime;
+        }
+    }
+}
